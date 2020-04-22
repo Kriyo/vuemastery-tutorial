0cde6b0922a2dbae5524d49b7681c0b4b736bcac
Kriyo <krikenn@gmail.com>
2020-04-22 17:40:18 +0100

    lesson 6 - class & style binding

---
+++ main.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    altText: "A pair of socks",
+    product: "Socks",
+    description: "A pair of warm fuzzy socks",
+    image: "./assets/vmSocks-green.png",
+    link: "https://www.amazon.com/s?k=socks",
+    inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polester", "Gender-neutral"],
+    variants: [
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/vmSocks-green.png",
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/vmSocks-blue.jpg",
+      },
+    ],
+    sizes: ["xs", "s", "m", "l", "xl", "xxl"],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    updateProduct: function (img) {
+      this.image = img;
+    },
+    removeFromCart: function () {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+  },
+});
